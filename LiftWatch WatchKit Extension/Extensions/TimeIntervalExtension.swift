9ce8193e51bc9cf910024370f0a3921db6d8333f
Sami Iljin <sami.iljin@op.fi>
2018-05-04 21:51:29 +0300

    Remove old shitty stuff

--- LiftWatch WatchKit Extension/Extensions/TimeIntervalExtension.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TimeIntervalExtension.swift
-//  LiftWatch WatchKit Extension
-//
-//  Created by Sami Iljin on 07/01/2018.
-//  Copyright © 2018 Sami Iljin. All rights reserved.
-//
-
-import Foundation
